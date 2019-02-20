30b2086c71ddf0e113cbd7161a0041af4b4858d7
Joe Neeman <joeneeman@gmail.com>
2019-02-20 14:54:39 -0600

    Prepare to release jp_multimap 0.1.0

---
+++ multimap/README.md
@@ -0,0 +1,4 @@
+This crate contains a barebones placeholder implementation of a multimap on top
+of a `BTreeMap` and a `BTreeSet`.  This is part of
+[`jp`](https://github.com/jneem/jp), and is not intended for public use. It
+will eventually be replaced by something disk-based and fully persistent.
--- multimap/src/lib.rs
+++ multimap/src/lib.rs
@@ -1,3 +1,14 @@
+// Copyright 2018-2019 Joe Neeman.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// See the LICENSE-APACHE or LICENSE-MIT files at the top-level directory
+// of this distribution.
+
 // This is just a hacked-up multimap. Eventually, we'll need to move to a fully persistent (in the
 // functional-data-structure sense), on-disk multimap.
 
