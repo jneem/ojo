009a2e55cc601cb9f3b04d791daa7b7275376fa6
Joe Neeman <joeneeman@gmail.com>
2019-02-20 19:35:15 -0600

    Prepare to release libjp 0.1.0

---
+++ libjp/README.md
@@ -0,0 +1,6 @@
+This is a rust crate for creating and interacting with `jp` repositories. For
+the command line interface to `jp`, see the `jp` crate.
+
+The API documentation for this crate is reasonably complete, but high-level
+documentation isn't. The best I have so far can be found in this [series of
+blog posts](https://jneem.github.io).
--- libjp/src/patch/change.rs
+++ libjp/src/patch/change.rs
@@ -1,3 +1,14 @@
+// Copyright 2018-2019 Joe Neeman.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// See the LICENSE-APACHE or LICENSE-MIT files at the top-level directory
+// of this distribution.
+
 use diff::LineDiff;
 
 use crate::storage::File;
