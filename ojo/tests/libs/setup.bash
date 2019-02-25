1b51d0999dcd48ef762f811a18ad8cd6b021842d
Joe Neeman <joeneeman@gmail.com>
2019-02-25 10:32:39 -0600

    Add CLI tests to circleci.

--- ojo/tests/libs/setup.bash
+++ ojo/tests/libs/setup.bash
@@ -5,6 +5,9 @@ setup() {
     export TOPLEVEL="$BATS_TEST_DIRNAME/../.."
     export OJO="$TOPLEVEL/target/debug/ojo"
 
+    # Ensure that the build is up-to-date.
+    cargo build --all
+
     # Run everything in a clean tmpdir.
     export TEST_WORKING_DIR=$(mktemp -d)
     cd "$TEST_WORKING_DIR"
